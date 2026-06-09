2dd6ac65a50c2446937024ab2bf7b96da0ecbf32
Vishwanath Akuthota <vishwanath.chintu@gmail.com>
2026-06-09 16:46:01 +0530

    161201 of experiments

--- openvals/__init__.py
+++ openvals/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.161201"
+__version__ = "0.3.0"
 
 from .core.evaluator import Evaluator
 from .benchmarking.benchmark import BenchmarkRunner
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="openvals",
-    version="0.3.161201",
+    version="0.3.0",
     author="Vishwanath Akuthota",
     description=(
 
